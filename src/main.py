d70488c3ff1f2895e63a27c8861f9c470b934ced
Zhibin Dai <zhibindai26@gmail.com>
2019-08-11 15:02:38 -0400

    OOP everything

--- src/main.py
+++ src/main.py
@@ -1,193 +1,38 @@
 #!/usr/bin/env python
 from datetime import datetime
-import math
-from utils import get_weather, get_trip_details, send_email
-from properties.constants import *
-
-ceil = math.ceil
-destination = get_trip_details.destination
-config = get_trip_details.config
-trip_length = get_trip_details.trip_length
-
-
-# create items dictionary
-def create_items_dict():
-    items_dict = {}
-    item_file = get_trip_details.item_list
-    with open("..\\csv\\" + item_file, "r") as infile:
-        next(infile)
-        for line in infile:
-            row = line.strip()
-            row = row.split(',')
-            item = row[0].strip()
-            category = row[1].strip()
-            items_dict[item] = category
-    return items_dict
-
-
-def non_clothes_items(items_dict):
-    with open(output_path, 'a') as outfile:
-        outfile.write('\n')
-        outfile.write('TOILETRIES')
-        outfile.write('\n')
-        for key, value in items_dict.iteritems():
-            if value == 'Toiletry':
-                outfile.write(checkbox + key.capitalize())
-                outfile.write('\n')
-
-        outfile.write('\n')
-        outfile.write('ELECTRONICS')
-        outfile.write('\n')
-        for key, value in items_dict.iteritems():
-            if value == 'Electronics':
-                outfile.write(checkbox + key.capitalize())
-                outfile.write('\n')
-
-        outfile.write('\n')
-        outfile.write('ACCESSORIES')
-        outfile.write('\n')
-        for key, value in items_dict.iteritems():
-            if value == 'Mandatory Accessories':
-                outfile.write(checkbox + key.capitalize())
-                outfile.write('\n')
-
-        if get_weather.rain is True or get_trip_details.trip_length > 4:
-            outfile.write(checkbox + 'Umbrella')
-            outfile.write('\n')
-        if get_weather.sunshine is True:
-            outfile.write(checkbox + 'Sunglasses')
-            outfile.write('\n')
-
-        if get_trip_details.international.lower() == 'yes':
-            outfile.write('\n')
-            outfile.write('ID')
-            outfile.write('\n')
-            for key, value in items_dict.iteritems():
-                if value == 'ID':
-                    outfile.write(checkbox + key.capitalize())
-                    outfile.write('\n')
-
-
-def regular_clothes(items_dict):
-    avg_temp = get_weather.avg_temp
-    avg_high = get_weather.avg_high
-    avg_low = get_weather.avg_low
-
-    clothes_dict = {}
-    for key, value in items_dict.iteritems():
-        if value == 'Clothes':
-            clothes_dict[key] = ''
-
-    clothes_dict['Boxers'] = trip_length + BOXERS
-    clothes_dict['Socks'] = trip_length + SOCKS
-    clothes_dict['Athletic Shorts'] = ceil(trip_length / ATHLETIC_SHORTS)
-    clothes_dict['Sweatpants'] = ceil(trip_length / SWEATPANTS)
-    clothes_dict['Inside Shirts'] = ceil(trip_length / INSIDE_SHIRTS)
-
-    # shirt counts
-    if avg_temp >= HOT:
-        clothes_dict['Collared Shirts'] = ceil(trip_length / HOT_CLOTHES["COLLARED SHIRTS"])
-        clothes_dict['Outside T-Shirts'] = ceil(trip_length / HOT_CLOTHES["OUTSIDE T SHIRTS"])
-        clothes_dict['Inside Shirts'] = ceil(trip_length / HOT_CLOTHES["INSIDE SHIRTS"])
-    elif WARM <= avg_temp < HOT:
-        clothes_dict['Collared Shirts'] = ceil(trip_length / HOT_WARM_CLOTHES["COLLARED SHIRTS"])
-        clothes_dict['Outside T-Shirts'] = ceil(trip_length / HOT_WARM_CLOTHES["OUTSIDE T SHIRTS"])
-    elif COOL <= avg_temp < WARM:
-        clothes_dict['Collared Shirts'] = ceil(trip_length / WARM_COOL_CLOTHES["COLLARED SHIRTS"])
-        clothes_dict['Outside T-Shirts'] = ceil(trip_length / WARM_COOL_CLOTHES["OUTSIDE T SHIRTS"])
-        clothes_dict['Sweaters'] = ceil(trip_length / WARM_COOL_CLOTHES["SWEATERS"])
-    elif CHILLY <= avg_temp < COOL:
-        clothes_dict['Collared Shirts'] = ceil(trip_length / COOL_CHILLY_CLOTHES["COLLARED SHIRTS"])
-        clothes_dict['Outside T-Shirts'] = 1
-        clothes_dict['Sweaters'] = ceil(trip_length / COOL_CHILLY_CLOTHES["SWEATERS"])
-    elif avg_temp <= COLD:
-        clothes_dict['Collared Shirts'] = ceil(trip_length / COLD_CLOTHES["COLLARED SHIRTS"])
-        clothes_dict['Sweaters'] = ceil(trip_length / COLD_CLOTHES["SWEATERS"])
-
-    # jeans counts
-    if avg_high >= WARM:
-        clothes_dict['Jeans'] = ceil(trip_length / HOT_CLOTHES["JEANS"])
-    else:
-        clothes_dict['Jeans'] = ceil(trip_length / COOL_CHILLY_CLOTHES["JEANS"])
-
-    # shorts counts
-    if avg_temp >= WARM:
-        clothes_dict['Outside Shorts'] = ceil(trip_length / HOT_WARM_CLOTHES["OUTSIDE SHORTS"])
-
-    # jacket counts
-    if avg_low <= COLD:
-        clothes_dict['Heavy Jacket'] = ceil(trip_length / COLD_CLOTHES["HEAVY JACKET"])
-    elif CHILLY <= avg_temp < COOL:
-        clothes_dict['Heavy Jacket'] = ceil(trip_length / COOL_CHILLY_CLOTHES["HEAVY JACKET"])
-        clothes_dict['Light Jacket'] = ceil(trip_length / COOL_CHILLY_CLOTHES["LIGHT JACKET"])
-    elif COOL <= avg_temp < WARM:
-        clothes_dict['Light Jacket'] = ceil(trip_length / WARM_COOL_CLOTHES["LIGHT JACKET"])
-
-    if get_trip_details.laundry == 'YES':
-        for key, value in clothes_dict.iteritems():
-            if clothes_dict[key] != '':
-                clothes_dict[key] = ceil(int(value) / 1.5)
-
-    # write to file
-    with open(output_path, 'a') as outfile:
-        outfile.write('CLOTHES')
-        outfile.write('\n')
-
-        sorted_clothes = []
-        for key, value in clothes_dict.iteritems():
-            if value != '':
-                sorted_clothes.append(key)
-
-        sorted_clothes.sort()
-        for i in sorted_clothes:
-            outfile.write(checkbox + i + ' - ' + str(clothes_dict[i]))
-            outfile.write('\n')
-
-        for key, value in items_dict.iteritems():
-            if avg_low <= COLD:
-                if value == 'Cold Accessories':
-                    outfile.write(checkbox + key)
-                    outfile.write('\n')
-
-            if get_trip_details.nice_clothes == 'YES':
-                if value == 'Formal Clothes':
-                    outfile.write(checkbox + key)
-                    outfile.write('\n')
-
-            if avg_high >= HOT and config.get('main', 'swimming').upper() == 'YES':
-                if value == 'Swimwear':
-                    outfile.write(checkbox + key)
-                    outfile.write('\n')
-
-            if value == 'Footwear':
-                outfile.write(checkbox + key)
-                outfile.write('\n')
-
+from utils import get_weather, get_trip_details, write_packing_list, send_email
 
 if __name__ == "__main__":
 
     from os.path import join as path_join
 
+    destination = get_trip_details.destination
+    trip_length = get_trip_details.trip_length
+    international = get_trip_details.international
+    travel_guide = get_trip_details.travel_guide
+
     output_file = destination + '_' + str(datetime.today().year) + '_packing.txt'
     output_path = path_join("trips", output_file)
-    checkbox = '[ ] '
+    checkbox = '[   ] '
+
+    weather = get_weather.GetWeather(destination, trip_length, international)
+    weather_details = weather.query_api()
 
     with open(output_path, 'w') as outfile:
         outfile.write('Packing List For ' + str(datetime.now().strftime("%B")) + ' '
-                      + str(datetime.today().year) + ' ' + destination + ' Trip')
+                      + str(datetime.today().year) + ' ' + destination.upper() + ' Trip')
         outfile.write('\n')
         outfile.write(str(int(trip_length)) + ' Days' + ' | ' +
-                      'Temps: Avg High: ' + str(get_weather.avg_high) + ', ' + 'Avg Low: ' + str(get_weather.avg_low))
+                      'Temps: Avg High: ' + str(weather_details["avg_high"]) + ', ' + 'Avg Low: '
+                      + str(weather_details["avg_low"]))
         outfile.write('\n')
         outfile.write('\n')
 
-    items_dct = create_items_dict()
-    regular_clothes(items_dct)
-    non_clothes_items(items_dct)
+    final_list = write_packing_list.WriteItems(get_trip_details, output_path, weather_details)
+    final_list.write_list()
 
-    if config.get('main', 'travel_guide').upper() == 'YES':
+    if travel_guide.lower() == 'yes':
         import pdfkit
-
         pdfkit.from_url('http://wikitravel.org/en/' + get_trip_details.destination_underscore,
                         destination + '_Travel_Guide' + '.pdf')
 
--- src/utils/get_weather.py
+++ src/utils/get_weather.py
@@ -4,77 +4,64 @@ import requests
 class GetWeather:
     def __init__(self, city, trip_length, international):
         self.international = international.lower()
-        self.trip_length = trip_length
+        self.city = city.lower()
+        self.trip_length = trip_length + 1
+        self.days = "&days=" + str(self.trip_length)
         self.api_key = "&key=5857cc9ae47b48278e8772d1ca50caa2"
         self.units = "&units=I"
         self.base_url = "https://api.weatherbit.io/v2.0/forecast/daily"
         if self.international == 'yes':
-            self.world_cities_dict = self.get_intl_cities_dict
-            self.search = "?city=" + city + "&country=" + self.world_cities_dict[city]
+            self.country_code = self.get_intl_ctry_code(self.city)
+            self.search = "?city=" + self.city + "&country=" + self.country_code[self.city]
         else:
-            self.us_cities_dict = self.get_us_cities_dict()
-            self.search = "?city=" + city + "," + self.us_cities_dict[city]
+            self.us_state = self.get_us_state(self.city)
+            self.search = "?city=" + self.city + "," + self.us_state
 
-        self.query_string = self.base_url + self.search + self.api_key + self.units
+        self.query_string = self.base_url + self.search + self.api_key + self.units + self.days
 
     @staticmethod
-    def get_intl_cities_dict():
-        world_cities = {}
+    def get_intl_ctry_code(city_to_search):
         with open("csv\\world_cities.csv", "r") as infile:
             for line in infile:
-                row = line.strip()
-                row = row.split(',')
-                city = row[0].strip()
-                code = row[5].strip()
-                world_cities[city] = code
-        return world_cities
+                row = line.strip().split(',')
+                city = row[0].strip().lower()
+                if city == city_to_search:
+                    return row[5].strip()
 
     @staticmethod
-    def get_us_cities_dict():
-        us_cities = {}
+    def get_us_state(city_to_search):
         with open("csv\\us_cities.csv", "r") as infile:
             for line in infile:
-                row = line.strip()
-                row = row.split(',')
-                city = row[0].strip()
-                state = row[1].strip()
-                us_cities[city] = state
-        return us_cities
+                row = line.strip().split(',')
+                city = row[0].strip().lower()
+                if city == city_to_search:
+                    return row[1].strip()
 
     def query_api(self):
         r = requests.get(self.query_string)
         if r.status_code == 200:
-            self.get_weather_details(r.json)
+            return self.get_weather_details(r.json())
         else:
             print("API request failed")
-        return r.status_code
+            return r.status_code
 
     def get_weather_details(self, weather_data):
-        # todo
-        # parse returned weather data json
-
-        avg_high_list = []
-        avg_low_list = []
-        precip_list = []
-        conditions_list = []
-        rain = False
-        sunshine = False
+        avg_high_list = [int(round(x["max_temp"])) for x in weather_data["data"]]
+        avg_low_list = [int(round(x["min_temp"])) for x in weather_data["data"]]
+        precip_list = [x["pop"] for x in weather_data["data"]]
+        if self.trip_length > 4 or max(precip_list) >= 20:
+            rain = True
+        else:
+            rain = False
 
         # calculate weather details
-        delete_from_avgs = -(len(avg_high_list) - int(self.trip_length) - 1)
-
-        if len(avg_high_list) > int(self.trip_length) and delete_from_avgs != 0:
-            del avg_high_list[delete_from_avgs:]
-            del avg_low_list[delete_from_avgs:]
-            del precip_list[delete_from_avgs:]
-            del conditions_list[delete_from_avgs:]
-
         avg_high = sum(avg_high_list) / float(len(avg_high_list))
         avg_low = sum(avg_low_list) / float(len(avg_low_list))
         avg_temp = (avg_low + avg_high) / 2
 
-        if max(precip_list) >= 20:
-            rain = True
-
-        if "Partly Cloudy" in conditions_list or "Clear" in conditions_list:
-            sunshine = True
+        return {
+            "rain": rain,
+            "avg_high": round(avg_high),
+            "avg_low": round(avg_low),
+            "avg_temp": round(avg_temp)
+        }
