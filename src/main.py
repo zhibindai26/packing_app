f29637c7098074292adf63d44b1670e65262c7a1
Zhibin Dai <zhibindai26@gmail.com>
2019-12-07 14:54:39 -0500

    add json configs

--- src/main.py
+++ src/main.py
@@ -6,13 +6,14 @@ from os.path import join as path_join
 
 if __name__ == "__main__":
     destination = get_trip_details.destination
-    travel_guide = get_trip_details.travel_guide
     item_list = get_trip_details.item_list.replace(".csv", "")
 
     year = datetime.today().year
     month = datetime.today().month
+    underscore = "_"
+    packing = "_packing.txt"
 
-    output_file = destination + '_' + item_list + '_' + str(year) + '_packing.txt'
+    output_file = destination + underscore + item_list + underscore + str(year) + packing
     output_path = path_join("trips", output_file)
 
     weather = get_weather.GetWeather(get_trip_details)
@@ -21,9 +22,4 @@ if __name__ == "__main__":
     final_list = write_packing_list.WriteItems(get_trip_details, output_path, year, month, weather_details)
     final_list.write_list()
 
-    if travel_guide.lower() == 'yes':
-        import pdfkit
-        pdfkit.from_url('http://wikitravel.org/en/' + get_trip_details.destination_underscore,
-                        destination + '_Travel_Guide' + '.pdf')
-
-    send_email.send_email(output_path)
+    # send_email.send_email(output_path)
